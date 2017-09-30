b0aeaa98b593469857f59dd3d7cfa81efd72d415
Courseratoolbox1 <Courseratoolbox1@users.noreply.github.com>
2017-09-29 20:19:39 -0400

    Beer problem - complete soln, but works only for small cases

--- pour.pl
+++ pour.pl
@@ -7,7 +7,26 @@ capacity(3, 3).
 capacity(4, 1).
 horizon(10).
 */
-
+/*
+vessels(4).
+source(1).
+people(3).
+capacity(1, 12).
+capacity(2, 3).
+capacity(3, 3).
+capacity(4, 2).
+horizon(10).
+*/
+vessels(5).
+source(1).
+people(3).
+capacity(1, 12).
+capacity(2, 3).
+capacity(3, 3).
+capacity(4, 3).
+capacity(5, 2).
+horizon(10).
+/*
 vessels(4).
 source(1).
 people(4).
@@ -16,9 +35,12 @@ capacity(2, 3).
 capacity(3, 3).
 capacity(4, 3).
 horizon(10).
-
+*/
 split() :-
 	constructList(Tray),
+	vessels(V),
+	people(P),
+	V >= P,
 	tryMovesWithDepth(Tray, 0).
 
 tryMovesWithDepth(Tray, D) :-
@@ -66,12 +88,66 @@ divide(Tray, NewTray, Depth, Limit) :-
  *
  * Parameters :
  * 		checkTray(Tray, BeerPerPerson).
- */
-checkTray([],_).
-checkTray([[_, X] | T], Value) :-
-	(X =:= 0 ; X =:= Value),
-	checkTray(T,Value).
+ */	
+checkTray(L, BeerPerPerson):-
+	assign(L, L1),
+	valid(L1, BeerPerPerson).
 	
+valid(Tray, BeerPerPerson):-
+	valid1(Tray, 1, BeerPerPerson).
+	
+valid1(_, K, _) :-
+	people(P),
+	K > P.
+	
+valid1(Tray, K, BeerPerPerson) :-
+	people(P),
+	K =< P,
+	findSum(Tray, K, BeerPerPerson),
+	K1 is K+1,
+	valid1(Tray, K1, BeerPerPerson).
+
+
+/* findSum - Given a PersonTray[Person, BeerAmount] and a Person
+ * number, find how much beer the person is assigned.
+ *
+ * Parameters :
+ * 		findSum(PersonTray, PersonNo, TotalBeer).
+ */
+findSum([], _, 0).
+findSum([[Id, Value]|T], Id, Sum) :-
+	findSum(T, Id, Sum1),
+	Sum is Sum1 + Value.
+	
+findSum([[X,_ ]|T], Id, Sum) :-
+	X \= Id,
+	findSum(T, Id, Sum).
+	
+	
+
+
+/* assign - Given a normal Tray, return a PersonTray where
+ * each vessel is assigned a person. The returned PersonTray
+ * has values of form [Person, BeerAmount].
+ *
+ * Parameters :
+ * 		(Tray, PersonTray).
+ */
+assign([],[]).
+assign([[_, Amt]|T], [[P, Amt] | T1]) :-
+	%hack, using capacity to enumerate people
+	capacity(X,_),
+	people(TotalP),
+	TotalP >= X,
+	P is X,
+	assign(T, T1).
+
+/* action - Given the Tray, src and dest, action tries
+ * to move the beer from Src to Destination and returns a new tray.
+ *
+ * Parameters :
+ * 		(Tray, Source, Destination, NewTray).
+ */
 action(Tray, Src, Dest, NewTray) :-
 	Src \= Dest,
 	beerIn(Tray, Src, SrcAmt),
